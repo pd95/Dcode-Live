0c19e3b2b22e473225d2b0ac60516f604056a3fe
Philipp <pd95@users.noreply.github.com>
2021-11-21 18:09:16 +0100

    Initial Commit

---
+++ Dcode/DcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DcodeApp.swift
+//  Dcode
+//
+//  Created by Philipp on 21.11.21.
+//
+
+import SwiftUI
+
+@main
+struct DcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
